7348a96cf08e804c80f8ffe67629dfe3a2d9d3fa
algalopez <algalopez85@gmail.com>
2020-09-19 01:31:56 +0200

    FunSpel: Add introduction

--- FunSpel/static/script.js
+++ FunSpel/static/script.js
@@ -10,11 +10,12 @@ if (document.readyState == STATE_LOADING) {
 
 function start() {
     document.getElementById("intro").addEventListener("click", function(){load_page('01_Intro.html')});
-    document.getElementById("concepts").addEventListener("click", function(){load_page('02_Concepts.html')});
-    document.getElementById("hof").addEventListener("click", function(){load_page('03_HOF.html')});
-    document.getElementById("recursion").addEventListener("click", function(){load_page('04_Recursion.html')});
-    document.getElementById("lenses").addEventListener("click", function(){load_page('05_Lenses.html')});
-    document.getElementById("bibliography").addEventListener("click", function(){load_page('06_Bibliography.html')});
+    document.getElementById("currying").addEventListener("click", function(){load_page('02_Currying.html')});
+    document.getElementById("recursion").addEventListener("click", function(){load_page('03_Recursion.html')});
+    document.getElementById("monads").addEventListener("click", function(){load_page('04_Monads.html')});
+    document.getElementById("hof").addEventListener("click", function(){load_page('05_HOF.html')});
+    document.getElementById("lenses").addEventListener("click", function(){load_page('06_Lenses.html')});
+    document.getElementById("bibliography").addEventListener("click", function(){load_page('07_Bibliography.html')});
  }
 
 function load_page(page) {
